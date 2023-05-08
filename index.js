b1fb637627ffc418ddc5cdce2d0c7097794e59f0
Harshit Daga <78150691+harshitdaga7@users.noreply.github.com>
2023-05-08 06:17:51 +0000

    added visualization

--- index.js
+++ index.js
@@ -639,9 +639,55 @@ function main(){
   DispNextOptions();
   displayfunc(webObjects.homepage);
 
+}
+
+// let OurObj={
+//   friends:[],
+//   traversal:[[0, 1]],
+//   email:''
+// }
+
+function drawGraph(){
+
+  let edges_list = [];
+  let nodes_list = [];
+
+  
 
 
 }
+
+// create data
+var data = {
+  nodes: [
+    {id: "Richard"},
+    {id: "Larry"},
+    {id: "Marta"},
+    {id: "Jane"},
+    {id: "Norma"},
+    {id: "Frank"},
+    {id: "Brett"}
+  ],
+  edges: [
+    {from: "Richard", to: "Larry"},
+    {from: "Richard", to: "Marta"},
+    {from: "Larry",   to: "Marta"},
+    {from: "Marta",   to: "Jane"},
+    {from: "Jane",    to: "Norma"},
+    {from: "Jane",    to: "Frank"},
+    {from: "Jane",    to: "Brett"},
+    {from: "Brett",   to: "Frank"}
+  ]
+};
+
+// create a chart and set the data
+var chart = anychart.graph(data);
+
+// set the container id
+chart.container("root");
+
+// initiate drawing the chart
+chart.draw();
 
 
 
